91348cdd20e27bdffa926221c2754ea987fc0f85
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-02-11 05:35:54 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33aa";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = "Rahul";
+// let score = "";
+
+/*
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+*/
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+/*
+let isLoggedIn = "Rahul";
+let isBooleanValue = Boolean(isLoggedIn);
+
+console.log(isBooleanValue);
+console.log(typeof isBooleanValue);
+*/
+
+//  1 =>  true; 0 => false
+// "" => false; "rahul" => true
+
+
+let someNumber = 33;
+
+let strinNumber = String(33);
+
+console.log(strinNumber);
+console.log(typeof strinNumber);
