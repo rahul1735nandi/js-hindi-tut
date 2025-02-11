68913c089ea5b87f18c0f771f51b0ba56dcb2566
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-02-11 15:59:29 +0000

    conversion of diffee=rent types

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -26,10 +26,52 @@ console.log(typeof isBooleanValue);
 //  1 =>  true; 0 => false
 // "" => false; "rahul" => true
 
-
+/*
 let someNumber = 33;
 
 let strinNumber = String(33);
 
 console.log(strinNumber);
 console.log(typeof strinNumber);
+*/
+
+// ************************ Operations *****************************
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+// console.log(typeof negValue);
+
+// let str1 = "Hello";
+// let str2 = " Rahul"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("2" + 1);
+// console.log(2 + "1");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y: ${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, y: ${b}`);
