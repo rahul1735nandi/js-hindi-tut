34b76c60ce33923def18cc7a6cc7208c79667acc
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-02-15 05:55:03 +0000

    Math and Number

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,42 @@
+/*
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const n1 = 123.8965;
+
+// console.log(n1.toPrecision(4));
+
+const n2 = 1000000;
+// console.log(n2.toLocaleString('en-IN'));
+
+*/
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.67));
+// console.log(Math.floor(4.67));
+// console.log(Math.ceil(4.24));
+// console.log(Math.sqrt(36));
+
+// console.log(Math.min(32,13,67,23,75));
+// console.log(Math.max(32,13,67,23,75));
+
+// console.log(Math.random());
+
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
