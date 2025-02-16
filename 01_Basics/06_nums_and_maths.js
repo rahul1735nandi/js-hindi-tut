cb1f0238996a8e464aa6bc32d97f9f6219a19880
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-02-16 06:12:06 +0000

    date

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -35,8 +35,14 @@ const n2 = 1000000;
 // console.log((Math.random() * 10) + 1);
 // console.log(Math.floor(Math.random() * 10) + 1);
 
-const min = 10;
-const max = 20;
+// const min = 10;
+// const max = 20;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.ceil(Math.random() * (max - min + 1)));
+
 
---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,48 @@
+let myDate = new Date();
+
+// console.log(myDate); // 2025-02-16T05:33:34.244Z
+// console.log(myDate.toString()); // Sun Feb 16 2025 05:33:34 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString()); // Sun Feb 16 2025
+// console.log(myDate.toISOString()); // 2025-02-16T05:33:34.244Z
+// console.log(myDate.toLocaleString()); // 2/16/2025, 5:33:34 AM
+// console.log(myDate.toLocaleDateString()); // 2/16/2025
+// console.log(myDate.toJSON()); // 2025-02-16T05:33:34.244Z
+// console.log(myDate.toLocaleTimeString()); // 5:33:34 AM
+// console.log(myDate.toTimeString()); // 05:33:34 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.to);
+
+let formatedDate = myDate.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric"
+});
+
+// console.log(formatedDate);
+
+// console.log(typeof myDate);
+
+// let createdDate = new Date(2025, 0, 9, 18, 4);
+// console.log(createdDate.toLocaleString());
+
+// let date1 = new Date("2025-01-14");
+let date1 = new Date("01-14-2025");
+// console.log(date1.toLocaleString());
+
+let timeStamp = Date.now();
+// console.log(timeStamp);
+// console.log(date1.getTime()); // provide miliseconds
+
+// console.log(Math.floor(Date.now()/1000)); // converting miliseconds to seconds
+
+let d2 = new Date();
+console.log(d2.getDay()); // return day sunday is 1, monday is 2 and so on
+console.log(d2.getMonth() + 1); // montth s start with 0 so we are addin +1.
+console.log(d2.getFullYear());
+
+let d3 = d2.toLocaleString('default', {
+    weekday: "narrow",  
+});
+
+console.log(d3);
