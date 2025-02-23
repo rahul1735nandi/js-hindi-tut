635ba29850fd72febcea8b81c02d9b238002adaf
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-02-23 14:54:28 +0000

    added some extra notes to objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,5 +1,5 @@
-// const tinderUser1 = new Object();
-const tinderUser1 = {};
+// const tinderUser1 = new Object(); // It's a singleton object
+const tinderUser1 = {}; // It's a non singleton object
 
 tinderUser1.id = "rr12";
 tinderUser1.name = "Rahul";
@@ -18,6 +18,7 @@ const regularUser = {
     }
 };
 
+// console.log(regularUser)
 // console.log(regularUser?.fullName)
 // console.log(regularUser?.fullName?.userFullName)
 // console.log(regularUser?.fullName?.userFullName?.firstName)
@@ -37,10 +38,11 @@ const obj4 = {
 };
 
 // const obj3 = {obj1, obj2};
+// console.log(obj3);
 
-// const obj3 = Object.assign({}, obj1, obj2, obj4) // The {} inside assign is optional but it's a good pratice to provide an {}.
+const obj3 = Object.assign({}, obj1, obj2, obj4) // The {} inside assign is optional but it's a good pratice to provide an {}.
 
-const obj3 = {...obj1, ...obj2, ...obj4};
+// const obj3 = {...obj1, ...obj2, ...obj4};
 // console.log(obj3);
 
 const users = [
@@ -59,10 +61,27 @@ const users = [
 ];
 
 // console.log(users[0].id);
-console.log(tinderUser1);
+// console.log(tinderUser1);
 
-console.log(Object.keys(tinderUser1));
-console.log(Object.values(tinderUser1));
-console.log(Object.entries(tinderUser1));
+// console.log(Object.keys(tinderUser1));
+
+// const newArr = Object.keys(tinderUser1);
+// newArr.push("email");
+// console.log(newArr);
+
+// console.log(Object.values(tinderUser1));
+
+// const newArr2 = Object.values(tinderUser1);
+// newArr2.push("rahul@outlook.com");
+// console.log(newArr2);
+
+
+// console.log(Object.entries(tinderUser1));
+
+// const newArr3 = Object.entries(tinderUser1);
+// const newData = ["gameId", "Ghoster"];
+// newArr3.push(newData);
+// console.log(newArr3);
 
 console.log(tinderUser1.hasOwnProperty('isloogedIn'))
+console.log(tinderUser1.hasOwnProperty('gameId'))
