bd1476971dfb95e0c6e47390237bf8ffad546879
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-02-24 13:40:25 +0000

    revison string

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -21,16 +21,18 @@ const gameName = new String('Ghoster-bgmi'); // another way to declare a string
 // const newStr = gameName.substring(0, 7); // end index character will not be included also we cannot provide negative values
 // console.log(newStr);
 
-/*
+
 // G    h    o    s    t    e    r    -    b    g    m     i
 // 0    1    2    3    4    5    6    7    8    9    10    11
 // -12 -11  -10  -9   -8   -7   -6   -5   -4   -3   -2    -1             
 // const anotherString = gameName.slice(0, 5);
-console.log(gameName.length);
+// console.log(gameName.length);
+// console.log(anotherString);
+// console.log(anotherString.length);
 
-const anotherString = gameName.slice(-5, 10);
+// const anotherString = gameName.slice(-5, 10);
+// const anotherString = gameName.slice(5, -3);
 console.log(anotherString);
-*/
 
 
 // const userName = "        user           ";
@@ -43,5 +45,35 @@ console.log(anotherString);
 // console.log(url.replace("%20", "-"));
 // console.log(url.includes("rahul"));
 
-const dummy = "rahul-nandi-debarati-biswas";
-console.log(dummy.split("-", 2));
+// const dummy = "rahul-nandi-debarati-biswas";
+// const myArr = dummy.split("-", 2)
+
+// console.log(myArr);
+// console.log(myArr.join(" "));
+
+
+const str = "rahulnandi";
+// const res = str.blink();
+// const res = str.bold();
+// const res = str.constructor();
+// const res = str.endsWith("i");
+// const res = str.padEnd(15, "-"); // The length of "rahulnandi" is 11 so it will add four "-" at the end of "rahulnandi" to match length of 15 and if the length is aleardy 15 or greater than it will do nothing
+
+// const res = str.padStart(15, "."); // same as padEnd but it will add "-" at the beginning
+
+// const res = str.isWellFormed;
+// const res = str.link();
+// console.log(res);
+
+// const paragraph = "The quick brown fox jumps over the lazy dog. It barked.";
+// const regex = /[A-Z]/g;
+// const regex = /[a-z]/g;
+// const regex = /[a-z]/i;
+// const found = paragraph.match(regex);
+
+// const hp = "Happy!"
+// const rp = `I am very ${hp.repeat(3)}`;
+// console.log(rp);
+
+// const sh = paragraph.search("quick");
+// console.log(sh);
