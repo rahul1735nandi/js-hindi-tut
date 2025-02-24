4ad8dcca9968d29e1ee611418bd2bcea10c8dc12
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-02-24 16:01:15 +0000

    revision nums and math

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -1,21 +1,39 @@
-/*
 const score = 400;
 // console.log(score);
 
 const balance = new Number(100);
 // console.log(balance);
+// console.log(typeof balance);
 
 // console.log(balance.toString());
-// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(3)); // how many numbers we want after decimal
 
-const n1 = 123.8965;
+// **Note: The .toFixed() method formats a number to a fixed number of decimal places and returns it as a string.
 
-// console.log(n1.toPrecision(4));
+const n1 = 123;
 
-const n2 = 1000000;
+// console.log("fixed => ",n1.toFixed(2));
+// console.log("pricision => ",n1.toPrecision(2));
+
+// console.log("fixed => ",n1.toFixed(4));
+/*
+=>  Ensures exactly 4 decimal places.
+=>  Adds trailing zeros if necessary.
+=>  Does not change the integer part.
+*/
+// console.log("pricision => ",n1.toPrecision(4));
+
+/*
+=> Ensures a total of 7 significant digits (not just decimals).
+=> 123 already has 3 significant digits, so 4 more digits are added as 
+   decimals.
+=> Does not force 7 decimal places like toFixed(7), but ensures the total 
+   precision.
+*/
+
+// const n2 = 1000000;
 // console.log(n2.toLocaleString('en-IN'));
 
-*/
 
 // ++++++++++++++++++++ Maths +++++++++++++++++++++++
 
@@ -43,6 +61,7 @@ const n2 = 1000000;
 const min = 1;
 const max = 6;
 
-console.log(Math.ceil(Math.random() * (max - min + 1)));
+// console.log(Math.ceil(Math.random() * (max - min + 1)));
 
 
+// console.log(Math.floor((Math.random() * (max - min + 1))) + min)
