adb53040e348f590da99182297b0da02e98cd886
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-03-03 05:07:03 +0000

    scope level

--- 03_Basics/02_scops.js
+++ 03_Basics/02_scops.js
@@ -6,10 +6,51 @@ if(true) {
     let a = 100;
     const b = 200;
     // var c = 300;
-    console.log("INNER: ",a)
-    console.log("INNER: ",b)
+    // console.log("INNER: ",a)
+    // console.log("INNER: ",b)
 }
 
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 // console.log(c)
+
+
+function one() {
+    const username = "rahul";
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+
+// one();
+
+if(true) {
+    const user = "rahul";
+    if(user === "rahul") {
+        const web = " youtube";
+        // console.log(user + web);
+    }
+    // console.log(web);
+}
+// console.log(user);
+
+// +++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++
+
+console.log(addOne(4));
+
+function addOne(num) {
+    return num + 1;
+}
+
+
+// addTwo();
+
+const addTwo = function(num) {
+    return num + 2;
+}
+
+// addTwo();
