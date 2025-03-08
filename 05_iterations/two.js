5a167773e626fc6734ff96425784c07fdbdd92b4
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-03-08 01:36:15 +0000

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0;
+
+// while (index <= 10) {
+//     console.log("Index: ",  index);
+//     index += 2;
+// }
+
+const myArray = ["Thor", "Hulk", "Ironman", "Spiderman", "Captain America"];
+let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+//     arr++;
+// }
+
+
+let score = 1;
+
+do {
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score <= 10);
