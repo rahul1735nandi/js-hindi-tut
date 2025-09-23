0fc421c499c7babbd122e9f9505984b7eed1513d
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-09-23 06:03:59 +0000

    continious and holey array with more description

--- 11_fun_with_js/test.js
+++ 11_fun_with_js/test.js
@@ -8,29 +8,48 @@
 // Double (float, string, function)
 
 
-const arrTwo = [1,2,3,4,5]; // Packed SMI Elements
+const arrTwo = [1,2,3,4,5]; // PACKED_SMI_ELEMENT
 
-arrTwo.push(6.0) // Packed Double Elements
+arrTwo.push(6.0) // PACKED_DOUBLE_ELEMENTS
 
-arrTwo.push("7") // Packed Elements
+arrTwo.push("7") // PACKED_ELEMENTS
 
-arrTwo[10] = 11; // Holey Elements
+arrTwo[10] = 11; // HOLEY_ELEMENTS
 
-// console.log(arrTwo);
-// console.log(arrTwo.length);
-// console.log(arrTwo[9]);
-// console.log(arrTwo[19]);
+console.log(arrTwo);
+console.log(arrTwo.length);
+console.log(arrTwo[9]);
+console.log(arrTwo[19]);
 
 // bound check
 // hasOwnProperty(arrTwo, 9);
-// hasOwnProperty(arrTwo.prototype, 10);
-// hasOwnProperty(Object.prototype, 10);
+// hasOwnProperty(arrTwo.prototype, 9);
+// hasOwnProperty(Object.prototype, 9);
 
 // holes are very expensive in JS
 
 const arrThree = [1,2,3,4,5];
-console.log(arrThree[2]);
+// console.log(arrThree[2]);
 
 // SMI > DOUBLE > PACKED
 
 // H_SMI > H_DOUBLE > H_PACKED
+
+const arrFour = new Array(3);
+// just 3 holes. HOLEY_SMI_ELEMENTS
+
+arrFour[0] = '1'; // HOLEY_ELEMENTS
+arrFour[1] = '2'; // HOLEY_ELEMENTS
+arrFour[2] = '3'; // HOLEY_ELEMENTS
+
+const arrFive = []; // SMI
+
+arrFive.push('1'); // PACKED_ELEMENTS
+arrFive.push('2');
+arrFive.push('3');
+
+const arrSix = [1, 2, 3, 4, 5]; // SMI
+arrSix.push(NaN) // PACKED_DOUBLE
+arrSix.push(Infinity) // PACKED_DOUBLE
+
+// for, for-of, forEach
