bd0d5cbeba1a16529cbbb55018499ebab7dc7f22
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-10-27 13:57:31 +0000

    keydown event

--- 08_events/practice1.js
+++ 08_events/practice1.js
@@ -22,5 +22,12 @@ function clicked() {
 // box.addEventListener('mouseenter', () => box.style.backgroundColor = 'pink');
 // box.addEventListener('mouseleave', () => box.style.backgroundColor = 'yellow');
 
-const para = document.getElementById('prg')
-para.addEventListener('dblclick', () => para.style.fontWeight = 'bold');
+// const para = document.getElementById('prg')
+// para.addEventListener('dblclick', () => para.style.fontWeight = 'bold');
+
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+input.addEventListener('keydown', (event) => {
+    output.textContent = input.value;
+})
