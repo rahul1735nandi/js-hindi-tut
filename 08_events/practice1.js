6277d547a082e75f45092c99f57b6e0f8806cc2b
rahul1735nandi <148409231+rahul1735nandi@users.noreply.github.com>
2025-10-27 05:56:23 +0000

    basic event practice

---
+++ 08_events/practice1.js
@@ -0,0 +1,26 @@
+function clicked() {
+    alert("Button Clicked");
+}
+
+// function changeText() {
+//     const para = document.getElementById('prg');
+//     para.innerText = "Hello, JavaScript!";
+// }
+
+
+// const btn = document.getElementById('btn').addEventListener('click', function() {
+//     document.getElementById('prg').textContent = "Hello, World!";
+// })
+
+
+// const btn = document.getElementById('btn').addEventListener('click', function () {
+//     document.body.style.backgroundColor = 'lightgreen';
+// })
+
+
+// const box = document.getElementById('box');
+// box.addEventListener('mouseenter', () => box.style.backgroundColor = 'pink');
+// box.addEventListener('mouseleave', () => box.style.backgroundColor = 'yellow');
+
+const para = document.getElementById('prg')
+para.addEventListener('dblclick', () => para.style.fontWeight = 'bold');
